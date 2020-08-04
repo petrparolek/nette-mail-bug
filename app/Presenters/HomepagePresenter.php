67ec5eefd085130c64c690579eb825e970d98bad
Petr Parolek <petr.parolek@gmail.com>
2020-08-04 13:37:51 +0200

    bugfix

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-
-use Latte\Engine;
+use Nette\Application\LinkGenerator;
+use Nette\Application\UI\ITemplate;
 use Nette\Localization\ITranslator;
 use Nette\Mail\IMailer;
 use Nette\Mail\Message;
 
 final class HomepagePresenter extends BasePresenter
 {
+
+    /**
+     * @var LinkGenerator
+     * @inject
+     */
+    public $linkGenerator;
 
     /**
      * @var IMailer
@@ -27,21 +33,25 @@ final class HomepagePresenter extends BasePresenter
 
 	public function renderDefault(): void
 	{
-        $latte = new Engine();
-
-        $latte->addFilter('translate', [$this->translator, 'translate']);
-
         $params = [
             'orderId' => 123,
         ];
+        $template = $this->createMailTemplate();
+        $html = $template->renderToString(__DIR__ . '/templates/email.latte', $params);
 
         $mail = new Message();
         $mail->setFrom('Franta <franta@example.com>')
             ->addTo('petr@example.com')
-            ->setHtmlBody(
-                $latte->renderToString(__DIR__ . '/templates/email.latte', $params)
-            );
+            ->setHtmlBody($html);
 
         $this->mailer->send($mail);
     }
+
+    private function createMailTemplate(): ITemplate
+    {
+        $template = $this->getTemplateFactory()->createTemplate();
+        $template->getLatte()->addProvider('uiControl', $this->linkGenerator);
+        return $template;
+    }
+
 }
